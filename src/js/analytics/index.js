4ea610b30b8f209b4551ca026b06c470f99530cc
Renzil DSouza <renzil@gmail.com>
2019-12-18 11:21:01 +0530

    Adding FB pixel events

--- src/js/analytics/index.js
+++ src/js/analytics/index.js
@@ -1,44 +1,126 @@
 import * as commonAnalytics from "../common/analytics";
 import * as modal from "../common/modal";
 
+function getProductData() {
+  var price = $("#product-buy-button").attr("data-price");
+  var gender = $(".o-product-gender-picker .o-button-toggle-text--selected").attr("data-gender");
+  var color = $(".o-product-color-picker .o-button-circle--selected").attr("data-color");
+  var size = $(".o-product-size-picker .o-button-toggle--selected").attr("data-size");
+
+  return {
+    price: price,
+    gender: gender,
+    color: color,
+    size: size
+  };
+}
 function addAnalytics() {
   $("#subscribe-modal .o-button-primary").on("click", function() {
+    var productData = getProductData();
+    fbq('track', 'CompleteRegistration', {
+      subscriptionType: modal.getSubscribeModalType(),
+      product_color: productData.color,
+      product_gender: productData.gender,
+      product_price: productData.price,
+      product_size: productData.size
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'SubscribeModal_Subscribe', 'value': modal.getSubscribeModalType() });
   });
 
   $("#hero-shop-now-link").on("click", function() {
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'hero_shopnow',
+      product_color: productData.color,
+      product_gender: productData.gender
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Hero_ShopNow', 'value': 1 });
   });
 
   $("#story-know-more-link").on("click", function() {
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'story_knowmore',
+      product_color: productData.color,
+      product_gender: productData.gender
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Story_KnowMore', 'value': 1 });
   });
 
   $("#gender-picker-male").on("click", function() {
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'product_gender_male',
+      product_color: productData.color,
+      product_price: productData.price
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Gender', 'value': 'male' });
   });
 
   $("#gender-picker-female").on("click", function() {
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'product_gender_female',
+      product_color: productData.color,
+      product_price: productData.price
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Gender', 'value': 'female' });
   });
 
   $(".o-product-color-picker .o-button-circle").on("click", function(e) {
-    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Color', 'value': $(e.target).attr("data-color") });
+    var color = $(e.target).attr("data-color");
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'product_color_' + color,
+      product_color: color,
+      product_gender: productData.gender,
+      product_price: productData.price,
+      product_size: productData.size
+    });
+    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Color', 'value': color });
   });
 
   $(".o-product-size-picker .o-button-toggle").on("click", function(e) {
-    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Size', 'value': $(e.target).attr("data-size") });
+    var size = $(e.target).attr("data-size");
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'product_size' + size,
+      product_color: productData.color,
+      product_gender: productData.gender,
+      product_price: productData.price,
+      product_size: size
+    });
+    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Size', 'value': size });
   });
 
   $("#sizing-chart-button").on("click", function(e) {
+    var productData = getProductData();
+    fbq('track', 'ViewContent', {
+      content_name: 'product_sizingchart',
+      product_color: productData.color,
+      product_gender: productData.gender,
+      product_price: productData.price,
+      product_size: productData.size
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_SizingChart', 'value': 1 });
   });
 
   $("#product-buy-button").on("click", function(e) {
-    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Price', 'value': $("#product-buy-button").attr("data-price") });
-    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Gender', 'value': $(".o-product-gender-picker .o-button-toggle-text--selected").attr("data-gender") });
-    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Color', 'value': $(".o-product-color-picker .o-button-circle--selected").attr("data-color") });
-    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Size', 'value': $(".o-product-size-picker .o-button-toggle--selected").attr("data-size") });
+    var productData = getProductData();
+    fbq('track', 'AddToCart', {
+      value: Number(productData.price),
+      currency: 'INR',
+      content_name: productData.gender + "_" + productData.color + "_" + productData.size,
+      product_color: productData.color,
+      product_gender: productData.gender,
+      product_price: productData.price,
+      product_size: productData.size
+    });
+    
+    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Price', 'value': productData.price });
+    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Gender', 'value': productData.gender });
+    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Color', 'value': productData.color });
+    gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Product_Buy_Size', 'value': productData.size });
   });
 }
 
--- src/js/common/analytics.js
+++ src/js/common/analytics.js
@@ -11,6 +11,10 @@ function onDocumentScroll() {
   $.each(scroll_depths, function(index, value) {
     if (percent >= value && maxScrollPercent < value) {
       maxScrollPercent = value;
+      fbq('track', 'ViewContent', {
+        content_name: 'scroll_depth',
+        scroll_depth: value
+      });
       gtag('event', 'scroll_change', { 'event_category': 'scroll', 'event_label': 'depth_percentage', 'value': value });
     }
   });
@@ -23,29 +27,53 @@ function addAnalytics() {
 
   // Header
   $(".o-header-link[href=\"#\"], .o-header-link[href=\"/\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'header_logo'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Header_Logo', 'value': 1 });
   });
   $(".o-header-link[href=\"#story\"], .o-header-link[href=\"/#story\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'header_story'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Header_Story', 'value': 1 });
   });
   $(".o-header-link[href=\"#shop\"], .o-header-link[href=\"/#shop\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'header_shop'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Header_Shop', 'value': 1 });
   });
   $(".o-header-link[href=\"#cart\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'header_cart'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Header_Cart', 'value': 1 });
   });
 
   // Footer
   $(".o-content-footernav-site a[data-type=\"home\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'footer_home'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Footer_Home', 'value': 1 });
   });
   $(".o-content-footernav-site a[data-type=\"contact\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'footer_contact'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Footer_Contact', 'value': 1 });
   });
   $(".o-content-footernav-site a[data-type=\"privacy\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'footer_privacy'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Footer_Privacy', 'value': 1 });
   });
   $(".o-content-footernav-site a[data-type=\"terms\"]").on("click", function() {
+    fbq('track', 'ViewContent', {
+      content_name: 'footer_terms'
+    });
     gtag('event', 'click_internal', { 'event_category': 'click', 'event_label': 'Footer_Terms', 'value': 1 });
   });
 }
